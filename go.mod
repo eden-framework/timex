9dccf2eacf3ee742ec38c21fb5a95cb2d937a791
liyiwen <johnnyeven@gmail.com>
2020-10-14 13:57:22 +0800

    init

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/eden-framework/timex
+
+go 1.14
---
+++ utils.go
@@ -0,0 +1,23 @@
+package timelib
+
+import (
+	"time"
+)
+
+const (
+	CSTZoneOffset = 8 * 60 * 60
+)
+
+func Now() time.Time {
+	return time.Now().In(CST)
+}
+
+var (
+	UTC *time.Location
+	CST *time.Location
+)
+
+func init() {
+	UTC = time.UTC
+	CST = time.FixedZone("CST", CSTZoneOffset)
+}
